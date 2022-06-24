00fc1338bdf37ceb95c29aea7d8a6d7753df33bf
Giovanna <giovannaelizz@hotmail.com>
2022-06-24 00:09:26 -0300

    middlewares problems 2

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import ProductController from './controllers/productController';
+// import * as validationProducts from './middlewares/validationProducts';
 
 const app = express();
 const productController = new ProductController();
 app.use(express.json());
 app.get('/products', productController.getAll);
-app.post('/products', productController.create);
+app.post(
+  '/products',
+  // validationProducts,
+  productController.create,
+);
 export default app;
